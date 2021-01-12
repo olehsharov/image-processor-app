ba7a42528869efdbb03c46219c9becf97efe50c9
Oleg Sharov <oleg.sharov@shtapel.com>
2021-01-12 11:14:25 +0200

    better worker stability

--- server/worker.js
+++ server/worker.js
@@ -7,20 +7,18 @@ const workers = process.env.WORKERS
 ;
 (async () => {
     console.log('Checking for new images...');
-    while (true) {
-        try {
-            var imageLibrary = new ImageLibrary(process.env.DATA_FOLDER);
-            var images = imageLibrary.list().filter(f => !f.settings);
-            while (images.length > 0) {
-                console.log(`Processing ${images.length}`);
-    
-                var jobs = times(workers, () => images.pop()).filter(img => img);
-                await Promise.all(jobs.map(j => imageLibrary.process(j)));
-    
-                await sleep(1000);
-            }
-        } catch (err) {
-            console.error('Error', err);
+    try {
+        var imageLibrary = new ImageLibrary(process.env.DATA_FOLDER);
+        var images = imageLibrary.list().filter(f => !f.settings);
+        while (images.length > 0) {
+            console.log(`Processing ${images.length}`);
+
+            var jobs = times(workers, () => images.pop()).filter(img => img);
+            await Promise.all(jobs.map(j => imageLibrary.process(j)));
+
+            await sleep(1000);
         }
+    } catch (err) {
+        console.error('Error', err);
     }
 })();
