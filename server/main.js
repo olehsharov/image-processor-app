cceb8aaa54274fa9308ae38b0dbe7b00045bd052
Oleg Sharov <oleg.sharov@shtapel.com>
2021-01-15 16:30:44 +0200

    EVEN MORE UNSAFE

--- server/main.js
+++ server/main.js
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
 
 
 app.use((req, res, next) => {
-    res.header('Content-Security-Policy', "object-src data: 'unsafe-eval'");
+    res.header('Content-Security-Policy', "default-src *  data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic'; object-src  * data: blob: 'unsafe-inline' 'unsafe-eval'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: 'unsafe-inline'; style-src * data: blob: 'unsafe-inline';font-src * data: blob: 'unsafe-inline';");
     next();
 })
 
